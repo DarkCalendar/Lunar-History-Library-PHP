0c387f37a59e99218f0945ae441ff10dad329bb4
DevNull-IR <97373457+DevNull-IR@users.noreply.github.com>
2023-05-20 12:22:41 +0330

    Add Lunar to Gregorian and Solar

--- lib/LunarDateService.php
+++ lib/LunarDateService.php
@@ -3,19 +3,26 @@
 namespace DarkCalendar\LunarDate;
 
 use DarkCalendar\SolarDate\SolarDateService;
-use JetBrains\PhpStorm\Pure;
 
 class LunarDateService
 {
     /**
      * @return SolarDateService
      */
-    private function solarDate(): SolarDateService
+    public function solarDate(): SolarDateService
     {
         return new SolarDateService();
     }
 
-    public function date($format, string|int $timestamp = '', string $time_zone = 'Asia/Baghdad', string $tr_num = 'fa'){
+    /**
+     * @param $format
+     * @param string|int $timestamp
+     * @param string $time_zone
+     * @param string $tr_num
+     * @return array|string
+     */
+    public function date($format, string|int $timestamp = '', string $time_zone = 'Asia/Baghdad', string $tr_num = 'fa'): array|string
+    {
         $T_sec = 0;
         if ($time_zone != 'local') date_default_timezone_set(($time_zone === '') ? 'Asia/Baghdad' : $time_zone);
         $ts = $T_sec + (($timestamp === '') ? time() : $this->tr_num($timestamp));
@@ -30,8 +37,8 @@ class LunarDateService
         $doy += $l_d;
         $out = '';
         $y = ($l_y % 30);
-        $kab = match ($y){
-            2, 5, 7, 10, 13, 16, 18, 21, 24, 26, 26 => 1,
+        $kab = match ($y) {
+            2, 5, 7, 10, 13, 16, 18, 21, 24, 26 => 1,
             default => 0
         };
         foreach ($format_extract as $sub) {
@@ -157,7 +164,7 @@ class LunarDateService
                     break;
 
                 case 't':
-                    $out .= ($l_m != 12) ? (31 - (int) ($l_m / 6.5)) : ($kab + 29);
+                    $out .= ($l_m != 12) ? (31 - (int)($l_m / 6.5)) : ($kab + 29);
                     break;
 
                 case 'U':
@@ -210,44 +217,126 @@ class LunarDateService
     }
 
     /**
+     * @param $year
+     * @return bool
+     */
+    function isLeapYearG($year): bool
+    {
+        return (($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0));
+    }
+
+    /**
+     * @param $year
+     * @return bool
+     */
+    public function isLeapYearL($year): bool
+    {
+        $leapYears = [2, 5, 7, 10, 13, 16, 18, 21, 24, 26, 29];
+        return in_array($year % 30, $leapYears);
+    }
+
+    /**
+     * @param string|int $l_y
+     * @param string|int $l_m
+     * @param string|int $l_d
+     * @param string $mod
+     * @return array|string
+     */
+    public function lunar_to_gregorian(string|int $l_y, string|int $l_m, string|int $l_d, string $mod = ''): array|string
+    {
+        $MonthDaysLunar = [0, 30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 29 + $this->isLeapYearL($l_y) ? 1 : 0];
+        if ($MonthDaysLunar[$l_m] < $l_d || $l_m > 12) return false;
+        $Leap = $this->isLeapYearL($l_y) == true ? 1 : 0;
+        $jd = (floor((11 * $l_y + 3) / 30) + (354) * $l_y + 30 * $l_m - floor(($l_m - 1) / 2) + $l_d + 1948440 - 385) + $Leap;
+
+        if ($jd > 2299160) {
+            $l = $jd + 68569;
+            $n = floor((4 * $l) / 146097);
+            // l -= round((146097 * n + 3) / 4);
+            $l -= round((146097 * $n + 3) / 4);
+            $i = floor((4000 * ($l + 1)) / 1461001);
+            $l -= floor((1461 * $i) / 4) - 31;
+            $j = floor((80 * $l) / 2447);
+            $D = $l - floor((2447 * $j) / 80);
+            $l = floor($j / 11);
+            $M = $j + 2 - 12 * $l;
+            $Y = 100 * ($n - 49) + $i + $l;
+            // return `${Y}-${M}-${D}a`;
+        } else {
+            $j = $jd + 1402;
+            $k = floor(($j - 1) / 1461);
+            $l = $j - 1461 * $k;
+            $n = floor(($l - 1) / 365) - floor($l / 1461);
+            $i = $l - 365 * $n + 29;
+            $j = floor((80 * $i) / 2447);
+            $D = $i - floor((2447 * $j) / 80);
+            $i = floor($j / 11);
+            $Y = 4 * $k + $n + $i - 4716;
+            $M = $j + 2 - 12 * $i;
+        }
+        if (empty($mod))
+            return [
+                (int)$Y, (int)$M, (int)$D,
+            ];
+        return implode($mod, [$Y, $M, $D]);
+    }
+
+    /**
+     * @param string|int $l_y
+     * @param string|int $l_m
+     * @param string|int $l_d
+     * @param string $mod
+     * @return array|string
+     */
+    public function lunar_to_solar(string|int $l_y, string|int $l_m, string|int $l_d, string $mod = ''): array|string
+    {
+        $to_gr = $this->lunar_to_gregorian($l_y, $l_m, $l_d);
+        return $this->solarDate()->gregorian_to_solar($to_gr[0], $to_gr[1], $to_gr[2], $mod);
+    }
+
+    /**
      * @param string|int $g_y
      * @param string|int $g_m
      * @param string|int $g_d
      * @param string $mod
      * @return array|string
      */
-    #[Pure] public function gregorian_to_lunar(string|int $g_y, string|int $g_m, string|int $g_d, string $mod = ''): array|string
+    public function gregorian_to_lunar(string|int $g_y, string|int $g_m, string|int $g_d, string $mod = ''): array|string
     {
         unset(func_get_args()[3]);
         $timestamp = strtotime(implode("-", func_get_args()));
-        list($d,$m,$y,$dayname,$monthname,$am)=explode(' ',date('d m Y D M a', $timestamp));
+        list($d, $m, $y) = explode(' ', date('d m Y D M a', $timestamp));
 
-        if (($y>1582)||(($y==1582)&&($m>10))||(($y==1582)&&($m==10)&&($d>14))) {
-            $jd=$this->ard_int((1461*($y+4800+$this->ard_int(($m-14)/12)))/4);
-            $jd+=$this->ard_int((367*($m-2-12*($this->ard_int(($m-14)/12))))/12);
-            $jd-=$this->ard_int((3*($this->ard_int(($y+4900+$this->ard_int(($m-14)/12))/100)))/4);
-            $jd+=$d-32076;
+        if (($y > 1582) || (($y == 1582) && ($m > 10)) || (($y == 1582) && ($m == 10) && ($d > 14))) {
+            $jd = $this->ard_int((1461 * ($y + 4800 + $this->ard_int(($m - 14) / 12))) / 4);
+            $jd += $this->ard_int((367 * ($m - 2 - 12 * ($this->ard_int(($m - 14) / 12)))) / 12);
+            $jd -= $this->ard_int((3 * ($this->ard_int(($y + 4900 + $this->ard_int(($m - 14) / 12)) / 100))) / 4);
+            $jd += $d - 32076;
         } else {
-            $jd = 367*$y-$this->ard_int((7*($y+5001 + $this->ard_int(($m-9)/7)))/4) + $this->ard_int((275*$m)/9)+$d+1729777;
+            $jd = 367 * $y - $this->ard_int((7 * ($y + 5001 + $this->ard_int(($m - 9) / 7))) / 4) + $this->ard_int((275 * $m) / 9) + $d + 1729777;
         }
-        $l=$jd-1948440+10632;
-        $n=$this->ard_int(($l-1)/10631);
-        $l=$l-10631*$n+355; // Correction: 355 instead of 354
-        $j=($this->ard_int((10985-$l)/5316)) * ($this->ard_int((50*$l)/17719)) + ($this->ard_int($l/5670)) * ($this->ard_int((43*$l)/15238));
-        $l=$l-($this->ard_int((30-$j)/15)) * ($this->ard_int((17719*$j)/50)) - ($this->ard_int($j/16)) * ($this->ard_int((15238*$j)/43))+29;
-        $m=$this->ard_int((24*$l)/709);
-        $d=$l-$this->ard_int((709*$m)/24);
-        $y=30*$n+$j-30;
+        $l = $jd - 1948440 + 10632;
+        $n = $this->ard_int(($l - 1) / 10631);
+        $l = $l - 10631 * $n + 355; // Correction: 355 instead of 354
+        $j = ($this->ard_int((10985 - $l) / 5316)) * ($this->ard_int((50 * $l) / 17719)) + ($this->ard_int($l / 5670)) * ($this->ard_int((43 * $l) / 15238));
+        $l = $l - ($this->ard_int((30 - $j) / 15)) * ($this->ard_int((17719 * $j) / 50)) - ($this->ard_int($j / 16)) * ($this->ard_int((15238 * $j) / 43)) + 29;
+        $m = $this->ard_int((24 * $l) / 709);
+        $d = $l - $this->ard_int((709 * $m) / 24);
+        $y = 30 * $n + $j - 30;
         if (empty($mod))
             return [
-                (int)$y,(int)$m,(int)$d,
+                (int)$y, (int)$m, (int)$d,
             ];
         return implode($mod, [$y, $m, $d]);
     }
 
-    function ard_int($float): float
+    /**
+     * @param $float
+     * @return float
+     */
+    public function ard_int($float): float
     {
-        return ($float < -0.0000001) ? ceil($float-0.0000001) : floor($float+0.0000001);
+        return ($float < -0.0000001) ? ceil($float - 0.0000001) : floor($float + 0.0000001);
     }
 
 
